97822c6fa8368acf589f4e941ec7335b5813f210
fake <fake@fake.fake>
2024-05-17 05:02:24 +0200

    responsive tweaking 1

--- index.js
+++ index.js
@@ -17,12 +17,11 @@ class Card{
     constructor( pair , containerId){
 	this.pair = pair;
         this.container = document.getElementById(containerId);
-	this.container.insertAdjacentHTML('beforeend','<div class="priceCard"><div><canvas id="'+this.pair+'" width="98%" height="95%"></canvas></div> '+this.pair+'</div>');
+	this.container.insertAdjacentHTML('beforeend','<div class="priceCard"><div><canvas id="'+this.pair+'" width="98%" height="95%"></canvas></div> '+this.pair+' : <u id='+this.pair+'price></u></div>');
 
     }
          
     
-
      makechart(){
    	
     const canvas = document.getElementById(this.pair);
@@ -65,11 +64,14 @@ this.chart = new Chart(canvas, {
 });
 		      }
 
-    updateChart(){
+    updateChart(startDate=this.startDate,endDate=this.endDate){
+	this.startDate = startDate;
+        this.endDate = endDate;
+	
 	let c = this;
         this.getPrices().then( function(){
 	    c.chart.data.datasets[0].data = c.prices ;
-
+            c.updatePrice();
 	    var color;
          if (c.prices[0] < c.prices[c.prices.length-1]){
              color = 'green';
@@ -94,8 +96,9 @@ this.chart = new Chart(canvas, {
     
     
     async getPrices(){
-    
-    let url = "https://api.polygon.io/v2/aggs/ticker/C:"+this.pair+"/range/1/day/"+this.startDate+"/"+this.endDate+"?adjusted=true&sort=asc&apiKey=uNUbKI4lCdBnOYr2le5M9K8iacpoD79b";
+	//Theres a problem with polygons's api ,it returns 2 elements less from the top
+	let url = "https://api.polygon.io/v2/aggs/ticker/C:"+this.pair+"/range/1/day/"+this.startDate+"/"+this.endDate+"?adjusted=true&sort=asc&apiKey=uNUbKI4lCdBnOYr2le5M9K8iacpoD79b";
+	console.log(url);
 let response = await fetch(url);
 
 if (response.ok) { // if HTTP-status is 200-299
@@ -114,18 +117,22 @@ if (response.ok) { // if HTTP-status is 200-299
 }
 
 }
+    updatePrice(){
+      var element = document.getElementById(this.pair+"price");
+	element.innerHTML = this.prices[this.prices.length-1];
+    }
 
     createChart(startDate=this.startDate,endDate=this.endDate){
 
 	this.startDate = startDate;
 	this.endDate = endDate;
 	let c = this;
-	
 	this.getPrices().then(function(){
 
            c.makechart()
+           c.updatePrice();
 
-
+           
 	},function(){}
 
 			     );
@@ -136,15 +143,33 @@ if (response.ok) { // if HTTP-status is 200-299
     
 }
 
+
+
+var first =  "2024-05-01";
+var last = "2024-05-10";
+
 const card = new Card("EURUSD",'container');
-card.startDate = "2024-05-01";
-card.endDate = "2024-05-10";
+const card1 = new Card("EURAUD",'container');
+const card2 = new Card("EURGBP",'container');
+const card3 = new Card("EURJPY",'container');
+
+card.createChart(first,last);
+card1.createChart(first,last);
+card2.createChart(first,last);
+card3.createChart(first,last);
 
 
-card.createChart();
 
 setTimeout(function(){
 
-    card.startDate = "2024-02-01";
-    card.endDate = "2024-02-10";
-    card.updateChart(); }, 3000);
+    var first = "2024-04-01";
+    var last = "2024-04-10";
+    card.updateChart(first,last);
+    card1.updateChart(first,last);
+    card2.updateChart(first,last);
+
+
+
+}, 3000000);
+
+
